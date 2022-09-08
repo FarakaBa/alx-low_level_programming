cd19cc5b0b65f4a8e04a4efc76de18c54dc34221
FarakaBa <bonniebaraka001@gmail.com>
2022-09-08 03:50:15 -0700

    Writing a program to print using outs(), which automacilly creates a new line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include<stdio.h>
+
+/**
+ * printig using puts()
+ * automaticaly creates a new line
+ */
+
 int main()
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 		return 0;
 }
