2c8fb007e84bdf24b083cdbbe4644a70ad1e5068
BBWorksB <bonnybaraka001@gmail.com>
2022-09-14 15:59:33 +0300

    summation of 3 and 5 multiples

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,4 +1,5 @@
 #include"main.h"
+#include <stdio.h>
 
 /**
  * main - entry point
@@ -16,7 +17,7 @@ int main(void)
 			sum += num;
 	}
 
-	printf("%d\n", sum);
+	printf ("%d\n", sum);
 
 	return (0);
 }
