2d7a787f541f00f4cfe3abb90570e00b92fcfb52
FarakaBa <bonniebaraka001@gmail.com>
2022-09-14 02:04:15 -0700

    check for alphabets

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,10 +3,11 @@
 /**
  * _isalpha - checks for alphabetic character
  * Return: 1 if c is a letter
- * 	otherwise 0
  */
 
 int _isalpha(int c)
 {
-	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+	if (c >= 97 && c <= 122 && c >= 65 c >= 90)
+		return (1);
+	return (0);
 }
